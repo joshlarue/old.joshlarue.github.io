f9d9f52da6e138c7d8596220ff41c2d4b9f96b9d
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-24 10:35:57 -0700

    changed image uris

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,12 @@ function App() {
   return (
       <div className="app-inner">
         <div className="icon-grid">
-          <GridIcon img='/react-portfolio/public/resume.png' desc='Resume' handleHover={handleHover} /> 
-          <GridIcon img='/react-portfolio/public/project.png' desc='proj1' handleHover={handleHover} />
-          <GridIcon img='/react-portfolio/public/project.png' desc='proj2' handleHover={handleHover} />
-          <GridIcon img='/react-portfolio/public/project.png' desc='proj3' handleHover={handleHover} />
-          <GridIcon img='/react-portfolio/public/project.png' desc='proj4' handleHover={handleHover} />
-          <GridIcon img='/react-portfolio/public/blog.png' desc='blog' handleHover={handleHover} />
+          <GridIcon img='/react-portfolio/tree/main/public/resume.png' desc='Resume' handleHover={handleHover} /> 
+          <GridIcon img='/react-portfolio/tree/main/public/project.png' desc='proj1' handleHover={handleHover} />
+          <GridIcon img='/react-portfolio/tree/main/public/project.png' desc='proj2' handleHover={handleHover} />
+          <GridIcon img='/react-portfolio/tree/main/public/project.png' desc='proj3' handleHover={handleHover} />
+          <GridIcon img='/react-portfolio/tree/main/public/project.png' desc='proj4' handleHover={handleHover} />
+          <GridIcon img='/react-portfolio/tree/main/public/blog.png' desc='blog' handleHover={handleHover} />
         </div>
         <div className="right-side">
           <h1>Check out my {currentHover}!</h1>
