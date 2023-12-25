20e451295d103befb7a2da98d2ec794ba50db975
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-25 10:53:04 -0700

    added coin sprite and animation

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
       <div className="app-inner">
         <div className="icon-grid">
-          <GridIcon img='/react-portfolio/resume.png' desc='Resume' handleHover={handleHover} /> 
+          <GridIcon img='/react-portfolio/resume.png' desc='resume' handleHover={handleHover} /> 
           <GridIcon img='/react-portfolio/project.png' desc='proj1' handleHover={handleHover} />
           <GridIcon img='/react-portfolio/project.png' desc='proj2' handleHover={handleHover} />
           <GridIcon img='/react-portfolio/project.png' desc='proj3' handleHover={handleHover} />
@@ -21,8 +21,9 @@ function App() {
         </div>
         <div className="right-side">
           <h1>Check out my {currentHover}!</h1>
-          <div className="image-wrapper">
-            <img src='/react-portfolio/headshot.png' />
+          <div className="images-wrapper">
+              <img id='coin-img' src='/react-portfolio/coin.png' />
+              <img id='headshot-img' src='/react-portfolio/headshot.png' />
           </div>
         </div>
       </div>
