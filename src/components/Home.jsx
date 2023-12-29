dc2936ebf377674ab9ea101671ccfa8d60afeab4
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-29 16:48:13 -0700

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ function Home(props) {
 
   return (
     <div className="right-side">
-      <h1 id='hero'>Check out my {props.currentHover}!</h1>
+      <h1 id='home-hero'>Check out my {props.currentHover}!</h1>
       <div className="images-wrapper">
           <img id='coin-img' src='/react-portfolio/coin.png' /> 
           <img id='headshot-img' src='/react-portfolio/headshot.png' />
