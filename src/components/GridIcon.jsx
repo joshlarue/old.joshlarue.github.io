e966a2fcbf1857945e80c528b636849e040feb79
joshlarue <44442031+joshlarue@users.noreply.github.com>
2023-12-29 12:17:50 -0700

    componentized grid icons for separate pages

--- src/components/GridIcon.jsx
+++ src/components/GridIcon.jsx
@@ -1,7 +1,9 @@
+import { Link } from "react-router-dom";
+
 function GridIcon(props) {
   return (
     <div className="icon" onMouseOver={() => props.handleHover(props.desc)} onMouseLeave={() => props.handleHover("website")}>
-      <a href='https://duckduckgo.com'><span class='icon-link'></span></a>
+      <Link to={`${props.desc == 'home' ? '/react-portfolio/' : '/react-portfolio/' + props.desc}`} ><span className='icon-link'></span></Link>
       <div className="icon-img" ><img src={props.img} /></div>
       <div className="icon-desc"><p>{props.desc}</p></div>
     </div>
--- src/components/Resume/Resume.jsx
+++
@@ -1,5 +0,0 @@
-function Resume() {
-  return <h1>Hello! This is my resume.</h1>
-}
-
-export default Blog;
