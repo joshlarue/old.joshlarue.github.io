cab73c886a091e7a5c408952961424ae0711ee2c
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-01-03 19:08:50 -0700

    updated svg and radial gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -18,15 +18,15 @@ function App() {
   return (
     <>
       <div className="app-inner">
-        <Routes>
-          <Route path='/react-portfolio/' element={<> <WideIcons handleHover={handleHover} /> <Home currentHover={currentHover}/></>} />
-          <Route path='/react-portfolio/resume/' element={<> <ThinIcons handleHover={handleHover} /> <Resume /> </>} />
-          <Route path='/react-portfolio/blog/' element={<> <ThinIcons handleHover={handleHover} /> <Blog /> </>} />
-          <Route path='/react-portfolio/proj1/' element={<> <ThinIcons handleHover={handleHover} /> </>} />
-          <Route path='/react-portfolio/proj2/' element={<> <ThinIcons handleHover={handleHover} /> </>} />
-          <Route path='/react-portfolio/proj3/' element={<> <ThinIcons handleHover={handleHover} /> </>} />
-        </Routes>
-      </div>
+          <Routes>
+            <Route path='/react-portfolio/' element={<> <WideIcons handleHover={handleHover} /> <Home currentHover={currentHover}/></>} />
+            <Route path='/react-portfolio/resume/' element={<> <ThinIcons handleHover={handleHover} /> <Resume /> </>} />
+            <Route path='/react-portfolio/blog/' element={<> <ThinIcons handleHover={handleHover} /> <Blog /> </>} />
+            <Route path='/react-portfolio/proj1/' element={<> <ThinIcons handleHover={handleHover} /> </>} />
+            <Route path='/react-portfolio/proj2/' element={<> <ThinIcons handleHover={handleHover} /> </>} />
+            <Route path='/react-portfolio/proj3/' element={<> <ThinIcons handleHover={handleHover} /> </>} />
+          </Routes>
+        </div>
     </>
   )
 }
