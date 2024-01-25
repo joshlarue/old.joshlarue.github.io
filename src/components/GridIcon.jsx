d4b52b9779784cd8fb3748d7cdaae65da1def146
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-01-24 22:50:40 -0700

    correcting links (again)

--- src/components/GridIcon.jsx
+++ src/components/GridIcon.jsx
@@ -5,17 +5,22 @@ function GridIcon(props) {
 
   switch (props.desc) {
     case 'home':
-      link = <Link to="/"><span className='icon-link'/></Link>
     case 'resume':
       link = <Link to="/resume"><span className='icon-link'/></Link>
     case 'blog':
-      link = <a href="/blog"><span className='icon-link' /></a>
+      
     default:
       <a href={`'/old-site/pages/' + ${props.desc}`}><span className="icon-link" /></a>
   }
-  props.desc ? 'resume' : link = <Link to="/resume"><span className='icon-link'/></Link>
-  props.desc ? 'movies.html' : link = <a href="/old-site/pages/movies.html"><span className='icon-link'/></a>
-
+  if (props.desc == 'resume') {
+    link = <Link to="/resume"><span className='icon-link'/></Link>
+  } else if (props.desc == 'blog') {
+    link = <a href="/blog"><span className='icon-link' /></a>
+  } else if (props.desc == 'home') {
+    link = <Link to="/"><span className='icon-link'/></Link>
+  } else {
+    link = <a href={`/old-site/pages/${props.desc}`}><span className='icon-link' /></a>
+  }
   return (
     <div className="icon" onMouseOver={() => props.handleHover(props.desc)} onMouseLeave={() => props.handleHover("website")}>
       <>{link}</>
