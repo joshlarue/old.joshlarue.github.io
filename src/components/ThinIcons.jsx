d0c4926e4a5fd81889dc0466f20715ac9879e135
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-01-24 23:10:59 -0700

    new images

--- src/components/ThinIcons.jsx
+++ src/components/ThinIcons.jsx
@@ -8,9 +8,9 @@ function ThinIcons(props) {
       <GridIcon img='/home.png'  desc='home' handleHover={props.handleHover} />
       <GridIcon img='/resume.png' desc='resume' handleHover={props.handleHover} />
       <GridIcon img='/blog.png' desc='blog' handleHover={props.handleHover}></GridIcon>
-      <GridIcon img='/project.png' desc='rockpaperscissors.html' handleHover={props.handleHover} />
-      <GridIcon img='/project.png' desc='movies.html' handleHover={props.handleHover} />
-      <GridIcon img='/project.png' desc='morse.html' handleHover={props.handleHover} />
+      <GridIcon img='/rockpaperscissors.png' desc='rockpaperscissors.html' handleHover={props.handleHover} />
+      <GridIcon img='/movies.png' desc='movies.html' handleHover={props.handleHover} />
+      <GridIcon img='/morse.png' desc='morse.html' handleHover={props.handleHover} />
       <GridIcon img='/project.png' desc='proj4' handleHover={props.handleHover} />
     </div>
   );
--- src/components/WideIcons.jsx
+++ src/components/WideIcons.jsx
@@ -6,10 +6,9 @@ function WideIcons(props) {
     <div className='icon-grid'>
     <GridIcon img='/blog.png' desc='blog' handleHover={props.handleHover} /> 
     <GridIcon img='/resume.png' desc='resume' handleHover={props.handleHover} />
-    <GridIcon img='/project.png' desc='rockpaperscissors.html' handleHover={props.handleHover} />
-    <GridIcon img='/project.png' desc='movies.html' handleHover={props.handleHover} />
-    <GridIcon img='/project.png' desc='morse.html' handleHover={props.handleHover} />
-    <GridIcon img='/project.png' desc='proj4' handleHover={props.handleHover} />
+    <GridIcon img='/rockpaperscissors.png' desc='rockpaperscissors.html' handleHover={props.handleHover} />
+    <GridIcon img='/movies.png' desc='movies.html' handleHover={props.handleHover} />
+    <GridIcon img='/morse.png' desc='morse.html' handleHover={props.handleHover} />
   </div>
   );
 }
