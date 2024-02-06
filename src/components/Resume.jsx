97fad976b93d6557325c841f99052b0c9fe4dd6a
joshlarue <44442031+joshlarue@users.noreply.github.com>
2024-02-05 22:07:03 -0700

    updated resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -16,15 +16,20 @@ function Resume() {
       <main>
         <section className='resume-section'>
           <h2>Education</h2>
+          <h3>SAIT</h3>
           <h5>Sept. 2023 - Present</h5>
           <p>Studying in the Software Development program at the SAIT School for Advanced Digital Technology.</p>
+          <h3>University of Alberta</h3>
+          <h5>September 2022 - April 2023</h5>
+          <p>Completed first year of BSc in Physiology with Specialization</p>
         </section>
         <section className='resume-section'>
-          <h2>Projects</h2>
+          <h2>Experience</h2>
           <a href="https://joshlarue.github.io" target='_blank'>My personal projects</a>
           <ul>
-              <li>Several web projects utilizing HTML, CSS, and vanilla JavaScript</li>
-              <li>SAIT CourSearch (placed third in a SAIT-run hackathon)</li>
+              <li>Several web projects utilizing HTML, CSS, and JavaScript with React</li>
+              <li>SAIT CourSearch: placed third in a SAIT-run hackathon</li>
+              <li>YYC-iosk: placed fourth in the YYCHacks 2024 hackathon</li>
               <li>Nextcloud home server</li>
           </ul>
         </section>
@@ -37,9 +42,8 @@ function Resume() {
           </ul>
         </section>
         <section className='resume-section'>
-          <h2>Career Goals</h2>
-          <p>I would love to work as a freelance web developer in the future. Working closely with clients
-              and being able to devote my entire time to their project's success is a goal of mine.
+          <h2>About me</h2>
+          <p id="resume-about">I am a developer with a passion for helping people create originative solutions to their problems. I am great at working collaboratively, love creating accessible software, and thrive in fast-paced environments.
           </p>
           </section>
       </main>
